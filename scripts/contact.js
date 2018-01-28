8d3064227738b61902ef4a9176064a57d2b1d5d9
Javier Garcia <JavierGarcia@MacBook-Air-de-Javier.local>
2018-01-28 13:21:34 +0100

    Practica finalizada 20180128

---
+++ scripts/contact.js
@@ -0,0 +1,31 @@
+document.getElementById("clase").addEventListener("click");
+document.getElementById("trabajo").addEventListener("click");
+document.getElementById("calle").addEventListener("click");
+document.getElementById("otros").addEventListener("click");
+
+function habilitar() {
+  document.getElementById("otro").style.visibility = "visible";
+}
+
+function deshabilitar() {
+  document.getElementById("otro").style.visibility = "hidden";
+}
+function wordCount() {
+  textoArea = document.getElementById("peticiones").value;
+  numeroCaracteres = textoArea.length;
+  inicioBlanco = /^ /;
+  finBlanco = / $/;
+  variosBlancos = /[ ]+/g;
+  textoArea = textoArea.replace(inicioBlanco, "");
+  textoArea = textoArea.replace(finBlanco, "");
+  textoArea = textoArea.replace(variosBlancos, " ");
+  textoAreaDividido = textoArea.split(" ");
+  numeroPalabras = textoAreaDividido.length;
+  if (numeroPalabras >= 151)
+    document.getElementById("peticiones").style.color = "red";
+  if (numeroPalabras < 151)
+    document.getElementById("peticiones").style.color = "black";
+
+  document.getElementById("palabras").innerHTML =
+    "Llevas " + numeroPalabras + " palabras escritas";
+}
